295933782dde3f0aac1c4a520bcd0b1f3be19c58
Wes Brown <wesbrown@Wess-MacBook-Pro.local>
2018-10-11 10:28:23 -0400

    Initial commit

---
+++ Algorithms/LinearSearch.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+public static class LinearSearch {
+
+	public static int Search (int [] array, int value) {
+		int index = 0;
+		int position = -1;
+		bool found = false;
+
+		while (index < array.Length && !found) {
+			if (array [index] == value) {
+				found = true;
+				position = index;
+			}
+
+			index++;
+		}
+
+		return position;
+	}
+
+	public static int Search (float [] array, float value)
+	{
+		int index = 0;
+		int position = -1;
+		bool found = false;
+
+		while (index < array.Length && !found) {
+			if (array [index].Equals7DigitPrecision (value)) {
+				found = true;
+				position = index;
+			}
+
+			index++;
+		}
+
+		return position;
+	}
+
+	public static int Search (string [] array, string value)
+	{
+		int index = 0;
+		int position = -1;
+		bool found = false;
+
+		while (index < array.Length && !found) {
+			if (array [index] == value) {
+				found = true;
+				position = index;
+			}
+
+			index++;
+		}
+
+		return position;
+	}
+}
---
+++ Algorithms/Tools/FloatExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+public static class FloatExtensions {
+	const float _3 = 0.001f;
+	const float _4 = 0.0001f;
+	const float _5 = 0.00001f;
+	const float _6 = 0.000001f;
+	const float _7 = 0.0000001f;
+
+	public static bool Equals3DigitPrecision (this float left, float right) {
+		return Math.Abs (left - right) < _3;
+	}
+
+	public static bool Equals4DigitPrecision (this float left, float right) {
+		return Math.Abs (left - right) < _4;
+	}
+
+	public static bool Equals5DigitPrecision (this float left, float right) {
+		return Math.Abs (left - right) < _5;
+	}
+
+	public static bool Equals6DigitPrecision (this float left, float right) {
+		return Math.Abs (left - right) < _6;
+	}
+
+	public static bool Equals7DigitPrecision (this float left, float right) {
+		return Math.Abs (left - right) < _7;
+	}
+
+}
