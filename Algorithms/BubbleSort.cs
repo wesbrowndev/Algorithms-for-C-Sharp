626cc9adfbd9bb3f2b6bc8a68f4b048109760a9f
wesbrowndev <wesbrowndev@gmail.com>
2018-10-11 21:53:29 -0400

    Added new algorithms
    Added bubble sort and selection sort.

---
+++ Algorithms/BubbleSort.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public static class BubbleSort {
+
+	public static void Sort (int [] array) {
+		for (int maxElement = array.Length - 1; maxElement > 0; maxElement--) {
+			for (int index = 0; index < maxElement; index++) {
+				if (array [index] > array [index + 1]) {
+					Swap (ref array [index], ref array [index + 1]);
+				}
+			}
+		}
+	}
+
+	static void Swap (ref int a, ref int b) {
+		int temp = a;
+		a = b;
+		b = temp;
+	}
+
+}
---
+++ Algorithms/SelectionSort.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+public static class SelectionSort {
+
+	public static void Sort (int [] array) {
+		int minIndex, minValue;
+
+		for (int start = 0; start < array.Length - 1; start++) {
+			minIndex = start;
+			minValue = array [start];
+
+			for (int index = start + 1; index < array.Length; index++) {
+				if (array [index] < minValue) {
+					minValue = array [index];
+					minIndex = index;
+				}
+			}
+
+			Swap (ref array [minIndex], ref array [start]);
+		}
+	}
+
+	static void Swap (ref int a, ref int b) {
+		int temp = a;
+		a = b;
+		b = temp;
+	}
+
+
+}
