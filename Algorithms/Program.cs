de0b3de5a91540a7a4350724b267044703b8bcb5
wesbrowndev <wesbrowndev@gmail.com>
2018-10-19 22:49:01 -0400

    Added Stacks
    Simple Stack

--- Algorithms/Program.cs
+++ Algorithms/Program.cs
@@ -3,7 +3,8 @@
 class Program {
 
 	static void Main (string [] args) {
-	
+
+		Console.WriteLine ("Hello, world!");
 	}
 
 }
---
+++ Algorithms/Stacks.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Stacks {
+
+	List<String> list = new List<string> ();
+
+	public void Push (string element) {
+		list.Add (element);
+	}
+
+	public void Pop () {
+		list.RemoveAt (list.Count);
+	}
+
+	public string Peek () {
+		return list.Count.ToString ();
+	}
+}
